572150f20165fe146af4262d33070e95f9811796
LeeZEun <je990420@ajou.ac.kr>
2023-09-27 18:58:38 +0900

    Update send message API

--- UserManager/src/main/java/com/buddle/UserManager/Entity/MessageInfo.java
+++ UserManager/src/main/java/com/buddle/UserManager/Entity/MessageInfo.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 @Table(name = "message_info")
 public class MessageInfo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(unique = true)
     private Long id;
 
