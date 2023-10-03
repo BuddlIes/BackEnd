a6890b39d7c8215d09224bac3a241bdb72b7dc95
LeeZEun <je990420@ajou.ac.kr>
2023-10-04 04:00:36 +0900

    Feat VolContentDto & Update VolListDto and VolService and VolListRepository

--- UserManager/src/main/java/com/buddle/UserManager/Repository/VolListRepository.java
+++ UserManager/src/main/java/com/buddle/UserManager/Repository/VolListRepository.java
@@ -1,7 +1,11 @@
 package com.buddle.UserManager.Repository;
 
+import com.buddle.UserManager.Entity.ChatListInfo;
+import com.buddle.UserManager.Entity.MessageInfo;
 import com.buddle.UserManager.Entity.VolunteerInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +15,10 @@ public interface VolListRepository extends JpaRepository<VolunteerInfo, Long> {
     Optional<VolunteerInfo> findById(Long volunteerId);
 
     List<VolunteerInfo> findAll();
+
+    List<VolunteerInfo> findTopByVolunteerIdOrderByWriteTime(Long volunteerId);
+
+    /* 완료된 봉사는 제거하는 쿼리 추가해야함! */
+    @Query("Select m from VolunteerInfo m where m.hashtag = :hashTag order by m.writeTime")
+    List<VolunteerInfo> findByhashTagOrderByWriteTime(@Param("hashTag") String hashTag);
 }
--- UserManager/src/main/java/com/buddle/UserManager/Service/VolService.java
+++ UserManager/src/main/java/com/buddle/UserManager/Service/VolService.java
@@ -1,7 +1,30 @@
 package com.buddle.UserManager.Service;
 
+import com.buddle.UserManager.Dto.ChatRoomDto;
+import com.buddle.UserManager.Dto.MsgDto;
+import com.buddle.UserManager.Dto.UserJoinRequestDto;
+import com.buddle.UserManager.Dto.VolListDto;
+import com.buddle.UserManager.Entity.MessageInfo;
+import com.buddle.UserManager.Entity.VolunteerInfo;
+import com.buddle.UserManager.Repository.VolListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VolService {
+    @Autowired
+    VolListRepository volListRepository;
+
+    public List<VolListDto> checkVolList(Long volunteerId, String hashTag) {
+        List<VolunteerInfo> volInfos = volListRepository.findTopByVolunteerIdOrderByWriteTime(volunteerId);
+        List<VolunteerInfo> volInfosHashtag = volListRepository.findByhashTagOrderByWriteTime(hashTag);
+
+        List<VolListDto> volDtoList = volInfos.stream().map(
+                m-> new VolListDto(m.getVolunteerId(),m.getWriter(),m.getHashtag(),m.getTitle(),m.getImg(),m.getWriteTime(),m.getVolTime(), m.getPlace())
+        ).collect(Collectors.toList());
+        return volDtoList;
+    }
 }
