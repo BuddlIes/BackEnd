205a3d8dbfe663ba0ba1cba3141f7971204176a9
sosongha3 <sosongha3@ajou.ac.kr>
2023-10-03 16:15:45 +0900

    Feat: stamp entity added

---
+++ UserManager/src/main/java/com/buddle/UserManager/Entity/StampAcquireInfo.java
@@ -0,0 +1,33 @@
+package com.buddle.UserManager.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "stampAcquire_info")
+public class StampAcquireInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Long acquire_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_number", referencedColumnName = "user_number")
+    private UserInfo userInfo;
+
+    @ManyToOne
+    @JoinColumn(name = "stamp_id", referencedColumnName = "stamp_id")
+    private StampInfo stampInfo;
+
+    @Column private Long acquire_time;
+
+}
