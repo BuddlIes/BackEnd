4b3b1cdae518e0a1885e317c3f6136edd8c17518
LeeZEun <je990420@ajou.ac.kr>
2023-10-11 00:10:18 +0900

    Upload VolUploadRequestDto Variable

--- UserManager/src/main/java/com/buddle/UserManager/Dto/VolUploadRequestDto.java
+++ UserManager/src/main/java/com/buddle/UserManager/Dto/VolUploadRequestDto.java
@@ -1,27 +1,28 @@
 package com.buddle.UserManager.Dto;
 
 import com.buddle.UserManager.Entity.UserInfo;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Column;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class VolUploadRequestDto {
-    private Long user_number;
 
-    private String user_name;
-    private String ajou_email;
-    private String login_id;
-    private String password;
-    private String user_nickname;
+    private Long volunteerId; //봉사 게시물 아이디
 
-    public UserInfo toEntity() {
-        UserInfo userInfo = new UserInfo();
-        userInfo.setUser_number(this.user_number);
-        userInfo.setName(this.user_name);
-        userInfo.setEmail(this.ajou_email);
-        userInfo.setPassword(this.password);
-        userInfo.setNickname(this.user_nickname);
-        return userInfo;
-    }
+    private Long writer; //게시물 작성자
+    private String title;
+    private String detailed;
+    private String hashtag; //해시태그
+    private String img; /* 타입 변경될 수 있음 */
+
+    private Long writeTime; //게시물 등록일시
+    private Long volTime; //봉사 요청일시
+    private String place;
 }
