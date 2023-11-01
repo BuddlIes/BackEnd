b0800aae2e145275ff08cc7cc9da44ee7f890af0
LeeZEun <je990420@ajou.ac.kr>
2023-11-01 20:47:46 +0900

    Update: Config

--- UserManager/src/main/java/com/buddle/UserManager/config/WebConfig.java
+++ UserManager/src/main/java/com/buddle/UserManager/config/WebConfig.java
@@ -1,15 +1,16 @@
-//package com.buddle.UserManager.Config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:8080");
-//    }
-//}
+package com.buddle.UserManager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://52.79.132.18:8443");
+    }
+}
