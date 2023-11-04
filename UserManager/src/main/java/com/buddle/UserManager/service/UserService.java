8606239625e70dbbcf48840b380a1dbc988b299a
LeeZEun <je990420@ajou.ac.kr>
2023-11-05 01:29:29 +0900

    Update: 회원가입 & 로그인 초기값 설정 완료

--- UserManager/src/main/java/com/buddle/UserManager/service/UserService.java
+++ UserManager/src/main/java/com/buddle/UserManager/service/UserService.java
@@ -17,6 +17,7 @@ public class UserService {
 
     //회원가입 확인
     public String join(UserJoinRequestDto users) {
+
         Optional<UserInfo> optUserId = userRepository.findById(users.getUser_number());
         Optional<UserInfo> optUserEmail = userRepository.findByEmail(users.getAjou_email());
         Optional<UserInfo> optWallet = userRepository.findByWallet(users.getWallet_address());
@@ -28,10 +29,14 @@ public class UserService {
             UserInfo userEntity = users.toEntity();
             userEntity.setDelete_yn(0); // 탈퇴 안함
             userEntity.setCreated_at(LocalDateTime.now()); //가입 일시
-            userEntity.setVol_hour(0F);
-            userEntity.setVol_num(0L);
-            userEntity.setTemperature(36.5);
-            userEntity.setCompliment_num(0L);
+            //userEntity.setRecent_aceess(LocalDateTime.now()); // null
+            userEntity.setModified_data(LocalDateTime.now()); //회원정보 수정일시
+
+            userEntity.setVol_hour(0F); //총 봉사 시간
+            userEntity.setVol_num(0L); //총 봉사 횟수
+
+            userEntity.setTemperature(36.5); //초기 봉사 온도
+            userEntity.setCompliment_num(0L); // 초기 칭찬받은 횟수
             userEntity.setAlarm(1); // 알람 ON
             userEntity.setLogin_num(0L); //로그인 횟수
 
@@ -45,12 +50,20 @@ public class UserService {
 
     //로그인 확인
     public String login(UserLoginRequestDto users) {
+
         Optional<UserInfo> optUserInfo = userRepository.findByEmail(users.getAjou_email());
+
         if(optUserInfo.isPresent()){ //이메일이 있으면
             UserInfo userInfo = optUserInfo.get();
 
             //데베에서 가져온 비번과 입력받은 비번 비교
-            if (userInfo.getPassword().equals(users.getPassword())) {
+            if (userInfo.getPassword().equals(users.getPassword())) { //로그인 성공
+                Long loginNumber = userInfo.getLogin_num() + 1; //로그인 횟수 1++
+                userInfo.setLogin_num(loginNumber);
+
+                userInfo.setRecent_access(LocalDateTime.now());
+
+                userRepository.save(userInfo);
                 return "Login Completed";
             }
             else {
