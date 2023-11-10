dd14174d0958fbb6d72994b7d1ba57caf99924a4
LeeZEun <je990420@ajou.ac.kr>
2023-11-10 23:57:19 +0900

    Feat: WebSocketConfig - STOMP

--- UserManager/src/main/java/com/buddle/UserManager/controller/ChatController.java
+++ UserManager/src/main/java/com/buddle/UserManager/controller/ChatController.java
@@ -4,13 +4,20 @@ import com.buddle.UserManager.dto.ChatRoomDto;
 import com.buddle.UserManager.dto.MsgDto;
 import com.buddle.UserManager.dto.SendMsgDto;
 import com.buddle.UserManager.service.ChatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//web socket STOMP 사용
 @RestController
+@RequiredArgsConstructor
 public class ChatController {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
     @Autowired
     ChatService chatService;
 
@@ -27,9 +34,16 @@ public class ChatController {
     }
 
     @PostMapping("/chat/send_message")
-    public String getSendMsg(@RequestBody SendMsgDto reqDto)
+    public void sendMsg(@RequestBody SendMsgDto reqDto, SimpMessageHeaderAccessor accessor)
     {
-        return chatService.sendMsg(reqDto);
+        simpMessagingTemplate.convertAndSend("/sub/chat/send_message" + reqDto.getChatroomid(), reqDto);
+
+//        return chatService.sendMsg(reqDto);
     }
+//    @PostMapping("/chat/send_message")
+//    public String sendMsg(@RequestBody SendMsgDto reqDto)
+//    {
+//        return chatService.sendMsg(reqDto);
+//    }
 
 }
--- UserManager/src/main/java/com/buddle/UserManager/dto/SendMsgDto.java
+++ UserManager/src/main/java/com/buddle/UserManager/dto/SendMsgDto.java
@@ -1,10 +1,7 @@
 package com.buddle.UserManager.dto;
 
 import com.buddle.UserManager.entity.MessageInfo;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +9,7 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class SendMsgDto {
 
     private Long chatroomid;
